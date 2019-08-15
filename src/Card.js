6a26bcdbacd6dd0272cae6063121095f2ae79b22
Patient Padawan <1PatientPadawan@gmail.com>
2019-08-15 09:50:30 -0700

    delete and add card buttons working

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,11 @@ import './card.css';
 function Card(props) {
     return (
         <div className="Card">
-            <button type="button">delete</button>
+            <button
+                onClick={() => props.onDeleteItem(props.id)}
+                type="button">
+                delete
+            </button>
             <h3>{props.title}</h3>
             <p>{props.content}</p>
         </div>
--- src/List.js
+++ src/List.js
@@ -3,7 +3,7 @@ import Card from './Card'
 import './list.css';
 
 function List(props) {
-     return (
+    return (
         <section className='List'>
             <header className='List-header'>
                 <h2>{props.header}</h2>
@@ -12,11 +12,14 @@ function List(props) {
                 {props.cards.map((card) =>
                     <Card
                         key={card.id}
+                        id={card.id}
                         title={card.title}
                         content={card.content}
+                        onDeleteItem={props.onDeleteItem}
                     />   
                 )}
                 <button
+                    onClick={() => props.onAddItem(props.id)}
                     type='button'
                     className='List-add-button'
                 >
@@ -25,6 +28,10 @@ function List(props) {
             </div>
         </section>    
     );
+}
+
+List.defaultProps = {
+    onAddItem: () => {},
 }
 
 export default List;
